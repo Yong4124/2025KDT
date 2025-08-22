af78f40430c90149a315ca97bcc51253eae963c3
Yong4124 <dndyd4323@naver.com>
2025-08-22 12:15:35 +0900

    FEAT: 250822 오전수업

---
+++ 250822Web/src/main/java/com/du/dto/Member.java
@@ -0,0 +1,19 @@
+package com.du.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Member {
+	
+	private String id;
+	private String pw;
+	private String name;
+	
+	
+
+}
