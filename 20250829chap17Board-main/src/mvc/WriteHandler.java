7d4e6737a147cc730cdce14226247ee10ae5dd89
Yong4124 <dndyd4323@naver.com>
2025-08-29 19:37:32 +0900

    FEAT: 250829수업

---
+++ 20250829chap17Board-main/src/mvc/WriteHandler.java
@@ -0,0 +1,17 @@
+package mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class WriteHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return "/WEB-INF/view/writeForm.jsp";
+	}
+	
+}
+
+	
