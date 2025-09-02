db5d81b88ce411e4c5a4cb71d231bbfcaf512997
Yong4124 <dndyd4323@naver.com>
2025-09-02 18:38:05 +0900

    FEAT : 250902 수업

---
+++ 250902Spring/src/main/java/spring/AppCtx.java
@@ -0,0 +1,26 @@
+package spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppCtx {
+
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao(); 
+	}
+	
+	@Bean
+	public MemberRegisterService memberRegSvc() {
+		return new MemberRegisterService(memberDao());
+	}
+	
+	@Bean
+	public ChangePasswordService changePwdSvc() {
+		ChangePasswordService pwdSvc = new ChangePasswordService();
+		pwdSvc.setMemberDao(memberDao());
+		return pwdSvc;
+	}
+	
+}
